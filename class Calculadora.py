8035683134a37616f5338f68fe2765a452cd18ba
asolisn <85768831+asolisn@users.noreply.github.com>
2021-07-27 15:15:58 -0500

    Add files via upload

---
+++ class Calculadora.py
@@ -0,0 +1,15 @@
+class Calculadora:
+    def__init__(self,numero1,numero2):
+         self.num1 = numero1
+         self,num2 = numero2
+         
+    def suma(self):
+        return self.num1 + self.num2
+    def resta(self)
+        pass
+    def multiplicacion(self):
+        multi = self.num1*self.num2
+        print("{}*{}={}".format(self.num1,self.num2,multi))
+        
+        def division(self):
+            
